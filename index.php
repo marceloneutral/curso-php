f06d7b27e581c956a1c6f6e624c7d4d020594772
marceloneutral <marceloneutralf2@gmail.com>
2020-07-27 03:07:07 -0300

    boraa

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+  <head>
+    <meta charset="utf-8">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="recursos/css/estilo.css">
+    <title>Curso PHP</title>
+  </head>
+    <body>
+      <header class="cabecalho">
+        <h1>Curso PHP</h1>
+        <h2>Índice dos Exercícios</h2>
+      </header>
+      <main class="principal">
+        <div class="conteudo">
+            <nav class="modulos">
+              <div class="modulo verde">
+                <h3>Básico</h3>
+                <ul>
+                      <li><a href="exercicio.php?dir=basico&file=ola">Olá PHP</a></li>
+                      <li><a href="exercicio.php?dir=basico&file=html">Integração HTML</a></li>
+                      <li><a href="exercicio.php?dir=basico&file=css">Integração CSS</a></li>
+                      <li><a href="exercicio.php?dir=basico&file=comentarios">Comentários CSS</a></li>
+                      <li><a href="exercicio.php?dir=basico&file=desafio">Dasafio</a></li>
+                </ul>
+              </div>
+              <div class="modulo vermelho">
+                <h3>Tipos</h3>
+                <ul>
+                      <li><a href="exercicio.php?dir=tipos&file=int">Tipo Inteiro</a></li>
+                      <li><a href="exercicio.php?dir=tipos&file=float">Tipo Float</a></li>
+                </ul>
+              </div>
+            </nav>
+        </div>
+      </main>
+      <footer class="rodape">
+        CLUTCH CAST CS © <?= date('Y') ?>
+      </footer>
+  </body>
+</html>
